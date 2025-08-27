e1b8afe719eaf069dbe07e02f330ed346be024cb
ire <ivar.refsdal@sikt.no>
2025-08-27 19:42:31 +0200

    WIP

--- labtail.sh
+++ labtail.sh
@@ -124,6 +124,8 @@ get_glab_output() {
     GLAB_OUTPUT='{}'
   elif [[ "$GLAB_OUTPUT" == *"connect: connection refused"* ]]; then
     GLAB_OUTPUT='{}'
+  elif [[ "$GLAB_OUTPUT" == *"error connecting to"* ]]; then
+    GLAB_OUTPUT='{}'
   else
     log_error "Unexpected exit code ${GLAB_EXIT_CODE} stdout/stderr was: ${GLAB_OUTPUT}"
     exit "${GLAB_EXIT_CODE}"
@@ -131,6 +133,30 @@ get_glab_output() {
 }
 
 LAST_PIPELINE_ID='INIT'
+LAST_TRACED_JOB_ID='INIT'
+
+maybe_trace_job() {
+  PIPELINE="$1"
+  if [[ "[]" == "$(echo "${PIPELINE}" | jq -r '.jobs')" ]]; then
+    log_error "should not happen"
+    exit 1
+  else
+    JOB_ID="$(echo "${PIPELINE}" | jq -r '.jobs[-1].id')"
+    JOB_URL="$( echo "${PIPELINE}" | jq -r '.jobs[-1].web_url')"
+    if [[ "$LAST_TRACED_JOB_ID" == "$JOB_ID" ]]; then
+      :
+    elif [[ "${JOB_ID}" == "null" ]]; then
+      log_error "jobs.id is null, cannot trace log"
+      sleep 1
+    else
+      log_info "tracing job ${JOB_ID} ..."
+      log_info "aka ${JOB_URL}"
+      glab ci trace "${JOB_ID}" || true
+      LAST_TRACED_JOB_ID="$JOB_ID"
+    fi
+  fi
+}
+
 while true; do
   get_glab_output
   PIPELINE="$( echo "$GLAB_OUTPUT" | { jq -r '.' 2>/dev/null || echo '{}'; })"
@@ -148,11 +174,19 @@ while true; do
       PIPELINE="$( echo "$GLAB_OUTPUT" | { jq -r '.' 2>/dev/null || echo '{}'; })"
       PIPELINE_ID="$(echo "${PIPELINE}" | jq -r '.id')"
       PIPELINE_STATUS="$(echo "${PIPELINE}" | jq -r '.status')"
+      JOB_ID="$(echo "${PIPELINE}" | jq -r '.jobs[-1].id')"
+      JOB_URL="$( echo "${PIPELINE}" | jq -r '.jobs[-1].web_url')"
 
       if [[ "${PIPELINE_ID}" == "${LAST_PIPELINE_ID}" ]]; then
-        if [[ "${PIPELINE_STATUS}" == "running" ]]; then
-          log_status "waiting for pipeline ${PIPELINE_ID} (\e[1;35m${PIPELINE_STATUS}\e[0m) to finish ..."
+        if [[ "[]" == "$(echo "${PIPELINE}" | jq -r '.jobs')" ]]; then
+          log_status "waiting for new job for pipeline ${PIPELINE_ID} ..."
           sleep 1
+        elif [[ "null" == "$(echo "${PIPELINE}" | jq -r '.jobs[-1].id')" ]]; then
+          log_status "waiting for new job for pipeline ${PIPELINE_ID} ..."
+          sleep 1
+        elif [[ "${PIPELINE_STATUS}" == "running" ]]; then
+          log_status "waiting for pipeline ${PIPELINE_ID} (\e[1;35m${PIPELINE_STATUS}\e[0m) to finish ..."
+          maybe_trace_job "$PIPELINE"
         elif [[ "${PIPELINE_STATUS}" == "pending" ]]; then
           log_status "waiting for pipeline ${PIPELINE_ID} (\e[1;33m${PIPELINE_STATUS}\e[0m) to finish ..."
           sleep 1
@@ -161,31 +195,30 @@ while true; do
         elif [[ "${PIPELINE_STATUS}" == "skipped" ]]; then
           log_status "waiting for new pipeline ..."
         elif [[ "${PIPELINE_STATUS}" == "failed" ]]; then
-          if [[ "[]" == "$(echo "${PIPELINE}" | jq -r '.jobs')" ]]; then
-            log_error "pipeline ${PIPELINE_ID} failed and no jobs available"
-            break
-          else
-            printf '\033[3J' # clear scrollback
-            printf '\033[2J' # clear whole screen without moving the cursor
-            printf '\033[H' # move cursor to top left of the screen
-            log_error "pipeline ${PIPELINE_ID} failed, dumping logs:"
-            JOB_ID="$(echo "${PIPELINE}" | jq -r '.jobs[-1].id')"
-            glab ci trace "${JOB_ID}"
-            JOB_URL="$( echo "$GLAB_OUTPUT" | jq -r '.jobs[-1].web_url')"
-            log_error "last job is ${JOB_URL}"
-            log_error "pipeline ${PIPELINE_ID} failed"
+          maybe_trace_job "$PIPELINE"
+          log_error "pipeline ${PIPELINE_ID} with job ${JOB_ID} failed"
+          log_error "failed job URL is ${JOB_URL}"
+#            printf '\033[3J' # clear scrollback
+#            printf '\033[2J' # clear whole screen without moving the cursor
+#            printf '\033[H' # move cursor to top left of the screen
+#            log_error "pipeline ${PIPELINE_ID} failed, dumping logs:"
+#            JOB_ID="$(echo "${PIPELINE}" | jq -r '.jobs[-1].id')"
+#            if [[ "${JOB_ID}" == "null" ]]; then
+#              log_error "jobs.id is null, cannot dump logs"
+#            else
+#              glab ci trace "${JOB_ID}" || true
+#              JOB_URL="$( echo "$GLAB_OUTPUT" | jq -r '.jobs[-1].web_url')"
+#              log_error "last job is ${JOB_URL}"
+#            fi
             break
           fi
         elif [[ "${PIPELINE_STATUS}" == "success" ]]; then
-          printf '\033[3J' # clear scrollback
-          printf '\033[2J' # clear whole screen without moving the cursor
-          printf '\033[H' # move cursor to top left of the screen
-          log_ok "pipeline ${PIPELINE_ID} succeeded. Dumping logs:"
-          JOB_ID="$(echo "${PIPELINE}" | jq -r '.jobs[-1].id')"
-          glab ci trace "${JOB_ID}"
-          JOB_URL="$( echo "$GLAB_OUTPUT" | jq -r '.jobs[-1].web_url')"
-          log_ok "last job is ${JOB_URL}"
-          log_ok "pipeline ${PIPELINE_ID} succeeded"
+#          printf '\033[3J' # clear scrollback
+#          printf '\033[2J' # clear whole screen without moving the cursor
+#          printf '\033[H' # move cursor to top left of the screen
+          maybe_trace_job "$PIPELINE"
+          log_info "pipeline ${PIPELINE_ID} with job ${JOB_ID} succeeded"
+          log_info "succeeded job URL is ${JOB_URL}"
           break
         else
           log_error "unhandled pipeline status '${PIPELINE_STATUS}'"
